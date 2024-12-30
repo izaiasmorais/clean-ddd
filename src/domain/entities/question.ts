97bee6c5bf5f8c4c4dbf2546f6e44850d02a9f67
Izaias Lima <izaiaslima356@gmail.com>
2024-12-29 22:45:09 -0300

    feat: create unique entity id class

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,14 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
+	authorId: UniqueEntityID;
+	bestAnswerId?: UniqueEntityID;
 	title: string;
 	content: string;
 	slug: string;
-	authorId: string;
+	createdAt: Date;
+	updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
